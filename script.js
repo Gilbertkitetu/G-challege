039392b56134ff33534e06caedbdf91f097307e0
Gilbert kitetu <gilbertkitetu2@gmail.com>
2020-12-09 19:36:04 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("Hello World;");
+var selectValue;
+var price = {
+    cup: 3,
+    plate: 5,
+    spoon: 1,
+    basin: 6,
+    thermos: 20,
+    trouser: 15,
+    tie: 5,
+    shirt: 16,
+    t_shirt: 10,
+    socks: 3
+};
+
+var check1 = document.querySelector('#cup');
+if (check1.checked) {
+
+}
