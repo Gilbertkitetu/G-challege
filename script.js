ed92db752e9119e790f25486febe33dd677a8c61
Gilbert kitetu <gilbertkitetu2@gmail.com>
2020-12-10 11:01:15 +0300

    added checkbox and popups

--- script.js
+++ script.js
@@ -1,19 +1,47 @@
 console.log("Hello World;");
-var selectValue;
-var price = {
-    cup: 3,
-    plate: 5,
-    spoon: 1,
-    basin: 6,
-    thermos: 20,
-    trouser: 15,
-    tie: 5,
-    shirt: 16,
-    t_shirt: 10,
-    socks: 3
-};
+// var price = {
+//     cup: 3,
+//     plate: 5,
+//     spoon: 1,
+//     basin: 6,
+//     thermos: 20,
+//     trouser: 15,
+//     tie: 5,
+//     shirt: 16,
+//     t_shirt: 10,
+//     socks: 3
+// };
 
-var check1 = document.querySelector('#cup');
-if (check1.checked) {
 
+// const cup = document.getElementById('cup').value;
+// cup.addEventListener('change', (event) => {
+//     if (event.target.checked) {
+//         alert('Checked');
+//     } else {
+//         alert('Not checked');
+//     }
+// });
+var popUp = document.getElementById("pop-up");
+var span = document.getElementsByClassName("close-popup")[0];
+
+function handleChange(e) {
+    const { checked } = e.target;
+    if ({ checked }) {
+        // alert("Checked");
+        document.getElementById("pop-up").style.display = "block";
+
+    } else {
+        alert("Not checked");
+    }
+    //alert("Checked");
+}
+
+function closePopUp() {
+    document.getElementById("pop-up").style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == popUp) {
+        document.getElementById("pop-up").style.display = "none";
+    }
 }
